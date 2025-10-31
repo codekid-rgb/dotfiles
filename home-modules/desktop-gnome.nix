1471bdde341540dc4cde1e99e6972155eb3aa59f
Eugene Lord <eugene@lord.ac>
2025-10-31 10:54:31 -0600

    chat gpt changes to desktop-gnome.nix to fix extension issues

--- home-modules/desktop-gnome.nix
+++ home-modules/desktop-gnome.nix
@@ -4,24 +4,24 @@
   config,
   ...
 }: {
-  config = lib.mkIf (config.roles.desktop.enable && config.roles.desktop.windowManager == "gnome") {
-    home.packages = with pkgs; [
-      # GNOME utilities
-      gnome-tweaks
-      dconf-editor
-      gnome-system-monitor
-      gnome-disk-utility
-      albert #like spolight but better in every way and it does not care about desktop
-      # GNOME extensions (commented out - uncomment as needed)
-      gnomeExtensions.pop-shell
-      gnomeExtensions.open-bar
-      gnomeExtensions.dash-to-dock
-      # gnomeExtensions.appindicator
-      # gnomeExtensions.clipboard-history
-      # gnomeExtensions.caffeine
-      gnomeExtensions.vitals
-      gnomeExtensions.hide-activities-button
-      gnomeExtensions.unite
+  options.roles.desktop.enable = lib.mkEnableOption "Desktop role";
+
+  config = lib.mkIf config.roles.desktop.enable {
+    home.packages = lib.mkMerge [
+      (lib.mkIf (config.roles.desktop.windowManager == "gnome") (with pkgs; [
+        gnome-tweaks
+        dconf-editor
+        gnome-system-monitor
+        gnome-disk-utility
+        albert
+        gnomeExtensions.pop-shell
+        gnomeExtensions.open-bar
+        gnomeExtensions.dash-to-dock
+        gnomeExtensions.vitals
+        gnomeExtensions.hide-activities-button
+        gnomeExtensions.unite
+      ]))
     ];
   };
 }
+
